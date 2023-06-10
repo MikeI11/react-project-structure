ef9e36e1a98ae00273c11ab009a7aa1974c48e0a
MikeI11 <132368319+MikeI11@users.noreply.github.com>
2023-06-10 19:48:48 +0300

    added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by <a href="http://www.mykhailo.com">Mykhailo Ivo</a> and is
+        <a href="https://github.com/MikeI11/react-project-structure" target="_blank" > open-sourced on Github</a>
+      </footer>
     </div>
   );
 }
