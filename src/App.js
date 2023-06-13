db9c7d30b9574dce0470875cfc1a9a0f58397294
MikeI11 <132368319+MikeI11@users.noreply.github.com>
2023-06-13 23:44:31 +0300

    added API integration(axios, useState)

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="New York" />
       <footer>
         This project was coded by <a href="http://www.mykhailo.com" target="_blank" rel="noreferrer">Mykhailo Ivo</a> and is
         <a href="https://github.com/MikeI11/react-project-structure" target="_blank" rel="noreferrer"> open-sourced on Github</a>
