dcc939e97410771016bf3f251edd35e96a3c9875
MikeI11 <132368319+MikeI11@users.noreply.github.com>
2023-06-15 21:29:47 +0300

    added first day forecast

--- src/Weather.js
+++ src/Weather.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 
 
 export default function Weather(props) {
@@ -50,6 +51,7 @@ if (weatherData.ready) {
                 </div>
             </form>
             <WeatherInfo data={weatherData} />
+            <WeatherForecast />
         </div>
     );
 } else {
